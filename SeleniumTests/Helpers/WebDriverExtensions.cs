93ae597a8a72dc25ce0122055a2e362af7e85e27
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-09-12 14:31:42 +0800

    imageWait and RandomAddress

--- SeleniumTests/Helpers/WebDriverExtensions.cs
+++ SeleniumTests/Helpers/WebDriverExtensions.cs
@@ -26,5 +26,23 @@ namespace SeleniumTests.Helpers
             return;
         }
 
+        public static void UntilLoadingDisappears(this WebDriverWait wait, IWebDriver driver)
+        {
+            wait.Until(d =>
+            {
+                var elements = d.FindElements(By.XPath("//*[@id='loading']/img"));
+                return elements.Count == 0 || !elements[0].Displayed;
+            });
+        }
+
+        public static void addressGens(this IWebDriver driver, string elementName)
+        {
+            Random rand = new Random();
+            var addressRand = rand.Next(10);
+            var element = driver.FindElement(By.Name(elementName));
+            element.Clear();
+            element.SendKeys("" + addressRand);
+            return;
+        }
     }
 }
