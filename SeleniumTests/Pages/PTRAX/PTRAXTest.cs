e4c38d0fea1571e93a5cb28e0b401b289d3cbb60
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-10-14 14:38:36 +0800

    App evaluation test

--- SeleniumTests/Pages/PTRAX/PTRAXTest.cs
+++ SeleniumTests/Pages/PTRAX/PTRAXTest.cs
@@ -55,22 +55,39 @@ namespace SeleniumTests
             wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='LinkButton1']"))).Click();
         }
 
-        public void AppEval()
+        public void AppEval(string AppNumber)
         {
+            string selColRecord = String.Concat("//td[contains(text(), '", AppNumber, "')]");
+            string chkBox = String.Concat("//td[contains(text(), '", AppNumber, "')]/parent::tr//input[@type='checkbox']");
+
+            driver.goToURL("http://192.168.20.71:1023/Account/DtraxLogin.aspx"); //Temporary for solo testing
             driver.selectElement("ctl00$ContentPlaceHolder1$ctlLogin1$txtUser", "evaluator");
             driver.selectElement("ctl00$ContentPlaceHolder1$ctlLogin1$txtPass", "P@ssw0rd");
             driver.FindElement(By.XPath("//*[@id='ContentPlaceHolder1_ctlLogin1_btnLogin']")).Click();
 
             // wait.Until(d => d.FindElement(By.XPath("//*[@id='gbox_grdMailbox_Procurement']")).Displayed);
-            // driver.FindElement(By.XPath("//td[contains(text(), 'NBP2509-00019')]")).Click();
-            // driver.FindElement(By.XPath("//td[contains(text(), 'NBP2509-00019')]/parent::tr//input[@type='checkbox']")).Click();
-            // driver.FindElement(By.XPath("//*[@id='MainContent_btnDocMgr_batchAcceptance']")).Click();
+            // driver.ClickElement(wait, selColRecord);
+            // driver.ClickElement(wait, chkBox);
+            // driver.ClickElement(wait, "//*[@id='MainContent_btnDocMgr_batchAcceptance']");
             // wait.Until(d => d.FindElement(By.XPath("/html/body/div[12]")).Displayed);
-            // driver.FindElement(By.XPath("//*[@id='MainContent_btnDocMgr_AcceptOk']")).Click();
+            // driver.ClickElement(wait, "//*[@id='MainContent_btnDocMgr_AcceptOk']");
             // IAlert alertReceive = driver.SwitchTo().Alert();
             // alertReceive.Accept();
             // wait.Until(d => d.FindElement(By.XPath("/html/body/div[12]")).Displayed);
+            // driver.FindElement(By.XPath("/html/body/div[13]/div[1]/a/span")).Click();
+
+            wait.Until(d => d.FindElement(By.XPath("//*[@id='gbox_grdMailbox_Procurement']")).Displayed);
+            driver.ClickElement(wait, selColRecord);
+            driver.ClickElement(wait, chkBox);
+            wait.Until(d => d.FindElement(By.Name("ctl00$MainContent$ctlDocMgr_OperatorsAdvice1$ddl_JumpTo_Steps")).Displayed);
+            // driver.selectDropdown("ctl00$MainContent$ctlDocMgr_OperatorsAdvice1$ddl_JumpTo_Steps", "257");
+            driver.selectDropdown("ctl00$MainContent$ctlDocMgr_OperatorsAdvice1$ddl_JumpTo_Steps", "Step 8 : BILLING (ISSUANCE OF ORDER OF PAYMENT)");
+            driver.ClickElement(wait, "//*[@id='btnJump']");
+            IAlert alertJump = driver.SwitchTo().Alert();
+            alertJump.Accept();
+            wait.Until(d => d.FindElement(By.XPath("/html/body/div[12]")).Displayed);
             driver.FindElement(By.XPath("/html/body/div[13]/div[1]/a/span")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='LinkButton1']"))).Click();
         }
 
         public void BillingEval()
